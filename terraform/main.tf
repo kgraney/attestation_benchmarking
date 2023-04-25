0c117e4828bbf1e2a011cf92af7b2b65c4af5269
Kevin Graney <kmg@google.com>
2023-04-24 22:01:01 -0400

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,242 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.49"
+    }
+  }
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "kmg" {
+  key_name   = "kmg_keypair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDfFO4peyUKMzoMW67opywcU/VnRfJpocMdneucDdNKWhhbxC9l8b2ihFUdEXxvzKE3iSeGn9PwUINCDYHbGX/KZIMX+KSg1wQFdtxk42kfzZhz/pxaSCdvhMXJiGnCD7VS1YMmF7ay2WaapqOBsjadx10BNXUDse1M9p9szDXC+3UHXKBnjNRjLB5ygWZECoYJ1+/GHaJf2NKxWokjI6G0wX5HOwbAkpH6iNxfZ3qXvzAxwJ+IzpGzqaU7+vqY5PARpHnfI+UEC/cyl9pnVjHs6skOJl7EF1ADXUrx2DPr3m1KOmqbQy4MENwuWGhxYL9kx42iejcS9iK0rk9tHBNzWNlPoqOPxrxibkG48MP83PJQmaVCyag5n7E1ZCJjNpMtdjTm7NXPcCaDxN98RXQzwDWpNAFECu0h0gGwtf018feMvUgidq4wOYGJp937Wyth7W0zks6gNmdba8KoD3+kxcjCdGfAqFTip+RUdAnlZLM5O5ErnxgXW4muA2016upWuP+SGy39e4oBa3TM+BqCW38OZNg+2AglweNNTZutWUA5iYBSsKQTER846yflZDo9J+weOdPylwAnTsiUcf3EzlxFxgZPtSZikhWQyFds9EVH8qx2zoP5reXVzilyGka9vkA0sUlWkRBqbH/NaItHFUh6pl31PFXXQ0dZhFyznQ== kmg@kmg.nyc.corp.google.com"
+}
+
+resource "aws_ecr_repository" "enclave_server_repository" {
+  name = "nsm_benchmark"
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_vpc" "enclave_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_internet_gateway" "enclave_internet_gateway" {
+  vpc_id = aws_vpc.enclave_vpc.id
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_subnet" "enclave_subnet" {
+  vpc_id = aws_vpc.enclave_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_route_table" "enclave_public_route" {
+  vpc_id = aws_vpc.enclave_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.enclave_internet_gateway.id
+  }
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_route_table_association" "enclave_route_table_assn" {
+  subnet_id      = aws_subnet.enclave_subnet.id
+  route_table_id = aws_route_table.enclave_public_route.id
+}
+
+resource "aws_default_security_group" "enclave_server_sg" {
+  vpc_id      = aws_vpc.enclave_vpc.id
+  tags = {
+    Name = "NsmBenchmark"
+  }
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port       = 2222
+    to_port         = 2222
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+
+data "template_file" "enclave_server" {
+  template = file("${path.module}/nitro_startup.sh")
+  vars = {}
+}
+
+resource "aws_iam_role" "enclave_ec2_role" {
+  name               = "nsm_benchmark_role"
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": ["ec2.amazonaws.com"]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "enclave_ec2_role_policy" {
+  name = "nsm_benchmarking_policy"
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "ec2:DescribeInstances",
+          "ecr:*",
+          "s3:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "enclave_server_profile" {
+  name = "nsm_benchmarking_profile"
+  role = aws_iam_role.enclave_ec2_role.name
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_iam_policy_attachment" "attach" {
+  name       = "enclave-attach"
+  roles      = ["${aws_iam_role.enclave_ec2_role.name}"]
+  policy_arn = "${aws_iam_policy.enclave_ec2_role_policy.arn}"
+}
+
+data "aws_ami" "enclave_server" {
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220316.0-x86_64-gp2"]
+  }
+}
+
+resource "aws_launch_template" "enclave_server" {
+  name = "nsm-benchmark"
+  image_id = data.aws_ami.enclave_server.id
+  #instance_type = "c5.xlarge"
+  user_data = base64encode(data.template_file.enclave_server.rendered)
+  #subnet_id = aws_subnet.enclave_subnet.id
+  key_name = aws_key_pair.kmg.id
+  monitoring {
+    enabled = true
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.enclave_server_profile.name
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+  enclave_options {
+    enabled = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = "NsmBenchmarking"
+  }
+}
+
+resource "aws_instance" "enclave_server2" {
+  ami = "ami-0c02fb55956c7d316"
+  count = 10
+  instance_type = "c6in.4xlarge"
+  user_data = data.template_file.enclave_server.rendered
+  enclave_options {
+    enabled = true
+  }
+  root_block_device {
+    volume_size = 32
+    volume_type = "gp3"
+  }
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.enclave_subnet.id
+  key_name = aws_key_pair.kmg.id
+  iam_instance_profile = aws_iam_instance_profile.enclave_server_profile.name
+  tags = {
+    Name = "NsmBenchmark"
+  }
+}
+
+#resource "aws_instance" "enclave_server3" {
+#  ami = "ami-0c02fb55956c7d316"
+#  count = 10
+#  instance_type = "c6a.4xlarge"
+#  user_data = data.template_file.enclave_server.rendered
+#  enclave_options {
+#    enabled = true
+#  }
+#  root_block_device {
+#    volume_size = 32
+#    volume_type = "gp3"
+#  }
+#  associate_public_ip_address = true
+#  subnet_id = aws_subnet.enclave_subnet.id
+#  key_name = aws_key_pair.kmg.id
+#  iam_instance_profile = aws_iam_instance_profile.enclave_server_profile.name
+#  tags = {
+#    Name = "NsmBenchmark"
+#  }
+#}
---
+++ terraform/nitro_startup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install tools.
+amazon-linux-extras install aws-nitro-enclaves-cli -y
+yum install aws-nitro-enclaves-cli-devel -y
+yum install amazon-ecr-credential-helper -y
+yum install python3 -y
+
+# Download custom build of recent socat with vsock support.
+# (The version in Amazon Linux is too old)
+su ec2-user -c 'aws s3 cp s3://kmg-ps-public/socat $HOME/socat'
+su ec2-user -c 'chmod +x $HOME/socat'
+
+# Setup Docker and the enclave config.
+usermod -aG ne ec2-user
+usermod -aG docker ec2-user
+echo "---
+memory_mib: 8096
+cpu_count: 4" > /etc/nitro_enclaves/allocator.yaml
+
+# Start Docker and Enclave services.
+systemctl start nitro-enclaves-allocator.service && sudo systemctl enable nitro-enclaves-allocator.service
+systemctl start docker && sudo systemctl enable docker
+
+# Build the enclave from ECR and launch it.
+su ec2-user -c 'eval $(aws ecr get-login --region us-east-1 --no-include-email) && docker pull 743396514183.dkr.ecr.us-east-1.amazonaws.com/nsm_benchmark:latest'
+su ec2-user -c 'nitro-cli build-enclave --docker-uri 743396514183.dkr.ecr.us-east-1.amazonaws.com/nsm_benchmark:latest --output-file $HOME/nsm_benchmark.eif'
+su ec2-user -c 'nitro-cli run-enclave --cpu-count 2 --enclave-cid 16 --memory 8096 --eif-path $HOME/nsm_benchmark.eif'
+
+# Forward SSH to the vsock.
+/home/ec2-user/socat TCP4-LISTEN:2222,reuseaddr,fork VSOCK-CONNECT:16:5006 &
+/home/ec2-user/socat TCP4-LISTEN:8000,reuseaddr,fork VSOCK-CONNECT:16:5005 &
+
+su ec2-user -c 'aws s3 cp s3://kmg-ps-public/upload_benchmark.sh $HOME/upload_benchmark.sh'
+su ec2-user -c 'chmod +x $HOME/upload_benchmark.sh'
+su ec2-user -c '$HOME/upload_benchmark.sh'
