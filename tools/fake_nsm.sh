4f2a50f85551c46d0fa2487a6d4634c9aa1748dc
Kevin Graney <kmg@google.com>
2023-05-03 09:55:29 -0400

    Update fake_nsm

--- tools/fake_nsm.sh
+++ tools/fake_nsm.sh
@@ -10,4 +10,5 @@ trap cleanup EXIT
 mkfifo --mode=0666 $device
 
 exec 3<>$device
-RUST_LOG=debug ./target/release/attestation_benchmarking 0<&3 1>&3
+#RUST_LOG=debug ./target/release/attestation_benchmarking 0<&3 1>&3
+python3 tools/echo.py 0<&3 1>&3
